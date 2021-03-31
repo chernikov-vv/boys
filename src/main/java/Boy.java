1c16429f58d079cfa7afcb8b1479ef2212a14468
chernikov_vv <vell39rus@gmail.com>
2021-04-01 00:14:39 +0300

    Initial commit

---
+++ src/main/java/Boy.java
@@ -0,0 +1,12 @@
+public class Boy {
+    private String name;
+    private int age;
+    Boy(final String name, final int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {return name;}
+    public int getAge() { return age;}
+    @Override
+    public String toString() { return name + "-" + age; }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static <T> Predicate<T> removeDuplicate(final Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
+    }
+
+
+    public static void main(final String[] args) {
+        final ArrayList<Boy> boys = new ArrayList<>() {{
+            add(new Boy("Николай", 68));
+            add(new Boy("Пётр", 53));
+            add(new Boy("Василий", 25));
+            add(new Boy("Михаил", 19));
+            add(new Boy("Алексей", 6));
+            add(new Boy("Николай", 86));
+            add(new Boy("Пётр", 35));
+            add(new Boy("Михаил", 111));
+            add(new Boy("Алексей", 22));
+            add(new Boy("Михаил", 1));
+            add(new Boy("Яков", 30));
+        }};
+        List<Boy> filteredBoys = boys.stream()
+                .filter(x -> (x.getAge() > 17))
+                .filter(removeDuplicate(Boy::getName))
+                .sorted(Comparator.comparing(Boy::getName))
+                .limit(4)
+                .collect(Collectors.toList());
+
+        System.out.println(filteredBoys);
+    }
+}
